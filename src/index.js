e376574ce4968c7810f4cabe5ba9a85b4606bd3a
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2021-08-19 17:49:12 +0300

    feat: do brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const BRACKETS_PAIRS = bracketsConfig.reduce((prev, curr) => {
+    prev[curr[0]] = curr[1];
+    return prev;
+  }, {});
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+    if (currentSymbol === BRACKETS_PAIRS[topElement]) {
+      stack.pop();
+    } else if (BRACKETS_PAIRS[currentSymbol]) {
+      stack.push(currentSymbol);
+    } else {
+      return false;
+    }
+  }
+  return !stack.length;
 }
